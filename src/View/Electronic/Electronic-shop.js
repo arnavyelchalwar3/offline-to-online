d8de31138b3b883c8c960bc805396ee6f6090cb4
gkusumbe7 <gkusumbe7@gmail.com>
2023-05-18 00:43:23 +0530

    added new code

--- src/View/Electronic/Electronic-shop.js
+++ src/View/Electronic/Electronic-shop.js
@@ -1,13 +1,20 @@
 import "./Electronic-shop.css"
 function Electronic(){
     return(<>
-      
       <div className="main-container">
            <p className="main-heading"> Eelectronic Shop</p>
-           <img src="./image/Ele-img/Eelectronic.jpg"  className="img"/>
+           <img src="./image/Ele-img/Eelectronic.jpg"  className="img" />
+      
+            <div className="Address-box" > 
+               <span className="Address-text"> Near Wadi 24034 ,  New City Nagpur</span>
+            </div>
 
+            <div class="Discout-box">
+               <span class="offer-text"> 50% <br/> OFF </span>
+            </div>
       </div>
    </>);
+
 }
 
 export default Electronic;
