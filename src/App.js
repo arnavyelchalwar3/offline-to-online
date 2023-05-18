52e8f40a0121ab3bf76a6b995a8f779e05d7b4e3
Leena200 <leenashahare90@gmail.com>
2023-05-18 23:04:34 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Form from './components/Form/Form';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
-import Jewellery from './View/Jewallary/Jewellery'
+import Jewellery from './View/Jewallary/Jewellery';
+import Fashion from './View/Fashion/Fashion'
+
 function App() {
   return (
    <>
-   <Jewellery/>
+   <Fashion/>
    </>
   );
 }
